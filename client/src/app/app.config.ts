1861b237337c669e1a4030a494a4886099dbe26b
Errieka Palanca <errieka.palanca@gmail.com>
2024-06-01 23:07:17 +0800

    feat: pagination to home page.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
   ],
 };
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -16,15 +17,27 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  totalRecords: number = 0;
+
+  rows: number = 5;
+
   onProductOutput(product: Product) {
     console.log(product, 'Output');
   }
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
 
-  ngOnInit() {
+  fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
       .subscribe((products: Products) => {
         this.products = products.items;
+        this.totalRecords = products.total;
       });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
